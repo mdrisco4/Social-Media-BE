54cb1451de310a835f6c9f91e6cfba5c214abe4f
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-07 17:17:22 -0500

    setting up models folder, Schemas & controllers.  working through error.

---
+++ controllers/posts.js
@@ -0,0 +1,19 @@
+// import PostMessage from "../models/postMessage.js";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const PostMessage = await PostMessage.find();
+
+        // console.log(postMessages);
+
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
+    res.send('It works');
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation')
+}
