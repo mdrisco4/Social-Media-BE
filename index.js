45befee6650d10ce2b9948f27aad9dd97faddaca
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-07 16:45:27 -0500

    set up DB with MongoDB and updated account.  Working on connecting to DB.

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 
-// sample comment
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = 'mongodb+srv://michaelndriscoll81:Gn94kXm8JGjVN424@cluster1.ic7h7be.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
